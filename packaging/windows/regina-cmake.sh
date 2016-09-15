c5140c0ea0fe8b6eb9094bc2c37c9248e31dea43
Benjamin Burton <bab@maths.uq.edu.au>
2016-09-16 06:38:05 +1000

    More mingw32 updates.

--- packaging/windows/regina-cmake.sh
+++ packaging/windows/regina-cmake.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/home/bab/software/lib/pkgconfig:/c/Program Files/Graphviz2.38/lib/pkgconfig"
 cmake -G 'MSYS Makefiles' -DQDBM=1 \
-  -DCMAKE_PREFIX_PATH=/c/Qt/Qt5.5.1/5.5/mingw492_32 \
+  -DCMAKE_PREFIX_PATH=/c/Qt/Qt5.7.0/5.7/mingw53_32 \
   -DCMAKE_INSTALL_PREFIX=/home/bab/software \
   ..
